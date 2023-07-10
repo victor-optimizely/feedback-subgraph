a524fb7ea50eb6d0fe63d62eecc7fb1de4f22db6
Victor <victor.hernandez@optimizely.com>
2023-07-09 20:10:10 -0400

    setup as subgraph

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,14 @@ import { ApolloServer } from '@apollo/server'
 import { typeDefinitions as typeDefs } from './types/types.js'
 import { ServerContext } from './types/ServerContext.js'
 import { resolvers } from './resolvers/resolvers.js'
+import { buildSubgraphSchema } from '@apollo/subgraph';
 
 export const createApolloServer = (options = {}) => {
     return new ApolloServer<ServerContext>({
-        typeDefs,
-        resolvers,
+        schema: buildSubgraphSchema({
+            typeDefs,
+            resolvers
+        }),
         ...options,
     })
 }
--- src/types/types.ts
+++ src/types/types.ts
@@ -4,6 +4,7 @@ import { FeedbackPageType } from "./FeedbackPage.type.js";
 import { mutations } from "./mutations.js";
 export const typeDefinitions = [
     `scalar Date`,
+    `extend schema @link(url: "https://specs.apollo.dev/federation/v2.0", import: ["@key", "@shareable"])`,
     FeedbackType,
     FeedbackPageType,
     queries,
